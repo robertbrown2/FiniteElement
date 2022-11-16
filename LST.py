ebc5034e7ed68cd10ace78a29329a73553b4b18a
robertbrown2 <47368553+robertbrown2@users.noreply.github.com>
2022-11-15 18:33:22 -0600

    Update LST.py

--- LST.py
+++ LST.py
@@ -333,36 +333,37 @@ def LST_plotSingle(xElem, yElem, u=None, D=None, minMax=None, output='VM', Nplot
   # Initialize meshgrid values
   # TODO Fix this
   [xi, eta] = meshgrid(linspace(-1, 1, Nplot), linspace(-1, 1, Nplot))
-  Z = zeros(shape(xi))
-  X = zeros(shape(xi))
-  Y = zeros(shape(xi))
-  
-  # Calculate plot values and locations
-  for i in range(Nplot):
-    for j in range(Nplot):
+  xi = xi.flatten()
+  eta = eta.flatten()
+  X = []
+  Y = []
+  Z = []
+  for i in range(len(xi)):
+    if (xi[i] + eta[i] <= 1.0):
+      Z = []
       if (output == 'J'):
-        Z[i,j] = linalg.det(LST_J(xElem, yElem, xi[i,j], eta[i,j]))
+        Z.append(linalg.det(LST_J(xElem, yElem, xi[i], eta[i])))
       elif (output == 'VM' or output == 'sigx' or output == 'sigy' or output == 'tauxy' or output == 'sig1' or output == 'sig2'):
-        Z[i,j] = LST_stress(xElem, yElem, u, xi[i,j], eta[i,j], D, type2D=type2D, output=output)
+        Z.append(LST_stress(xElem, yElem, u, xi[i], eta[i], D, type2D=type2D, output=output))
       elif (output == 'epsx' or output == 'epsy' or output == 'gammaxy'):
-        eps = LST_strain(xElem, yElem, u, xi[i,j], eta[i,j], type2D=type2D)
+        eps = LST_strain(xElem, yElem, u, xi[i], eta[i], type2D=type2D)
         if (output == 'epsx'):
-          Z[i,j] = eps[0]
+          Z.append(eps[0])
         elif (output == 'epsy'):
-          Z[i,j] = eps[1]
+          Z.append(eps[1])
         else:
-          Z[i,j] = eps[2]
+          Z.append(eps[2])
       else:
         print('Output type', output, ' not supported')
         raise Exception
-      [X[i,j], Y[i,j]] = LST_map(xd, yd, xi[i,j], eta[i,j])
+    [xval, yval] = LST_map(xd, yd, xi[i], eta[i])
+    Y.append(yval)
+    X.append(xval)
   
   # Plot things
   if (undeformedLines):
-    x=xElem.copy()
-    x.append(xElem[0])
-    y=yElem.copy()
-    y.append(yElem[0])
+    x=[xElem[0], xElem[1], xElem[2], xElem[0]]
+    y=[yElem[0], yElem[1], yElem[2], yElem[0]]
     plot(x, y, 'k--')
   if (deformedLines):
     plot([xd[0], xd[5], xd[1], xd[3], xd[2], xd[4], xd[0]], 
