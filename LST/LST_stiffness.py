da9da294c5446e348c0ad95b1128ba0384483f71
robertbrown2 <47368553+robertbrown2@users.noreply.github.com>
2022-11-16 07:50:37 -0600

    Create LST_stiffness.py

---
+++ LST/LST_stiffness.py
@@ -0,0 +1,24 @@
+def LST_stiffness(xElem, yElem, xi, eta, D, thickness=None, type2D='planeStress'):
+  """
+  Calculate the stiffness matrix for a Q4 element
+  Usage - K = Q4_stiffness(xElem, yElem, xi, eta, D, thickness)
+  ---------
+    Input
+  ---------
+  xElem - (list) x locations of nodes
+  yElem - (list) y locations of nodes
+  xi - (float) position in unmapped element
+  eta - (float) position in unmapped element
+  D - (array) constituitive matrix
+  thickness - (float) thickness of element in third dimension
+  """
+  from numpy import array
+  from .LST_B import LST_B
+  from .LST_J import LST_J
+  
+  B = LST_B(xElem, yElem, xi, eta, type2D)
+  if (type2D == 'axisymmetric'):
+    psi = LST_shapeFunctions(xi, eta)
+    thickness = 2*pi*(array(xElem) @ array(psi))
+  Area = linalg.det(LST_J)
+  return Area*thickness*(transpose(B)@D@B)
