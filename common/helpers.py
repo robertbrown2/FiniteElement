52c2d7a70a4ea5550e0e691e3e1ac1701e7ee72e
robertbrown2 <47368553+robertbrown2@users.noreply.github.com>
2022-11-16 07:45:26 -0600

    Create helpers.py

---
+++ common/helpers.py
@@ -0,0 +1,24 @@
+def connIndex(conn):
+  # Find the minimum index in a list of lists
+  minVal = conn[0][0]
+  for elem in conn:
+    minVal = min(minVal, min(elem))
+  return minVal
+
+def sameLines(line1, line2):
+  # Determine if two lists have the same nodes, even if they are in a different order
+  # Specifically for lists that have two elements
+  if (line1[0]==line2[0] and line1[1]==line2[1]):
+    return True
+  elif (line1[0]==line2[1] and line1[1]==line2[0]):
+    return True
+  else:
+    return False
+
+def whichLine(newLine, lineList):
+  # Find the index of a line from a list of lines
+  # Returns -1 if not found
+  for i, oldLine in enumerate(lineList):
+    if (sameLines(newLine, oldLine)):
+      return i
+  return -1
