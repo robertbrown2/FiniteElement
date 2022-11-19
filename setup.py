b57979f134bf71a7d5930dd3c6443928a1860a93
robertbrown2 <47368553+robertbrown2@users.noreply.github.com>
2022-11-18 21:37:38 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='FiniteElement',
+    version='0.1',
+    author='Robert Brown',
+    description='Simple Finite Element Solver',
+    url='https://github.com/robertbrown2/FiniteElement',
+    python_requires='>=3.8',
+    install_requires=['numpy'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License'
+    ],
+    py_modules=[],
+    test_suite='tests',
+)
