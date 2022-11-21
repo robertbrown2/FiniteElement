0a15d6efbfe03df27bf35403ffe918122cdf3bd4
robertbrown2 <47368553+robertbrown2@users.noreply.github.com>
2022-11-21 14:20:39 -0600

    Update meshRefine.py

--- common/meshRefine.py
+++ common/meshRefine.py
@@ -31,18 +31,18 @@ def meshRefine(xnode, ynode, conn):
           xmid = (xnode[line[0]-index] + xnode[line[1]-index])/2
           ymid = (ynode[line[0]-index] + ynode[line[1]-index])/2
           iLine = len(LSTLineList)
-          LSTLineList.append(line)
-          LSTxnode.append(xmid)
-          LSTynode.append(ymid)
+          LineList.append(line)
+          xnodeNew.append(xmid)
+          ynodeNew.append(ymid)
         lineNodes.append(iLine+len(xnode)+index)
         
       # 1-6-5
-      LSTconn.append([elem[0], lineNodes[2], lineNodes[1]])
+      connNew.append([elem[0], lineNodes[2], lineNodes[1]])
       # 2-4-6
-      LSTconn.append([elem[1], lineNodes[0], lineNodes[2]])
+      connNew.append([elem[1], lineNodes[0], lineNodes[2]])
       # 3-5-4
-      LSTconn.append([elem[2], lineNodes[1], lineNodes[0]])
+      connNew.append([elem[2], lineNodes[1], lineNodes[0]])
       # 4-5-6
-      LSTconn.append([lineNodes[0], lineNodes[1], lineNodes[2]])
+      connNew.append([lineNodes[0], lineNodes[1], lineNodes[2]])
   
   return [xnodeNew, ynodeNew, connNew]
