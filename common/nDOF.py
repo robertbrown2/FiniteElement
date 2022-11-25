e40a1635e4f92e5f2b68bdcc8842da7c6b264773
robertbrown2 <47368553+robertbrown2@users.noreply.github.com>
2022-11-24 21:12:40 -0600

    Update nDOF.py

--- common/nDOF.py
+++ common/nDOF.py
@@ -1,5 +1,9 @@
 def nDOF(type2D):
   """
-  Currently returns 2, as all implemented 2D solids currently have 2 DOF.
+  Diffusion has 1 DOF (T / concentration)
+  Other solids currently have 2 DOF
   """
-  return 2
+  if (type2D == 'diffusion'):
+    return 1
+  else:
+    return 2
