3342fc7ebfa9f8a9c3a51b21bb6c0f829f85f5c4
robertbrown2 <47368553+robertbrown2@users.noreply.github.com>
2022-11-24 20:32:38 -0600

    Added diffusion matrix.

--- common/constMatrix.py
+++ common/constMatrix.py
@@ -1,4 +1,4 @@
-def constMatrix(E, nu, type2D):
+def constMatrix(E=None, nu=None, type2D='planeStress', k=None):
   """
   Create the constituitive matrix for a 2D solid.
   Usage - D = constMatrix(E, nu, type2D)
@@ -10,12 +10,13 @@ def constMatrix(E, nu, type2D):
   type2D: (string) - assumption for 2D solid
           'planeStress' - stress in z direction is zero (thin plates)
           'planeStrain' - strain in z direction is zero (thick bodies)
+  k: (float) - thermal diffusivity of material
   ----------
     Output
   ----------
   D: (array) - constituitve matrix
   """
-  from numpy import array
+  from numpy import array, eye
   if (type2D == 'planeStress'):
     D = E / (1 - nu**2) * array([
                              [1, nu, 0],
@@ -35,8 +36,10 @@ def constMatrix(E, nu, type2D):
                              [nu, nu, 1-nu, 0],
                              [0, 0, 0, (1-2*nu)/2]
                             ])
+  elif (type2D == 'diffusion'):
+    D = k * eye(2)
   else:
-    print('type2D must be "planeStress", "planeStrain", or "axisymmetric".  Was instead: ', type2D)
+    print('type2D must be "planeStress", "planeStrain", "axisymmetric", or "diffusion".  Was instead: ', type2D)
     raise Exception
 
   return D
