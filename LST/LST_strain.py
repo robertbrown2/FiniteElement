0c80be334b7b197b5f5d6e60b1d8f191a7e116f7
robertbrown2 <47368553+robertbrown2@users.noreply.github.com>
2024-11-11 17:06:45 -0600

    Update LST_strain.py
    Fix axisymmetric stress

--- LST/LST_strain.py
+++ LST/LST_strain.py
@@ -19,17 +19,27 @@ def LST_strain(xElem, yElem, u, xi, eta, type2D='planeStress', output=None):
   tauxy: (float) shear strain
   """
   from .LST_B import LST_B
+  from numpy import any, max
 
+  # If a value in x goes to 0, it breaks the axisymmetric stress.
+  if type2D == 'axisymmetric' and any(xElem < max(xElem) * 1e-3):
+    for i, x in xElem:
+      if xElem[i] < max(xElem) * 1e-3:
+        xElem[i] = max(xElem) * 1e-3
+  
   B = LST_B(xElem, yElem, xi, eta, type2D)
+  
   eps = B @ array(u)
   if (output == None):
     return eps
-  elif (output == 'epsx'):
+  elif (output == 'epsx' or output == 'epsr'):
     return eps[0]
-  elif (output == 'epsy'):
+  elif (output == 'epsy' or output == 'epsz'):
     return eps[1]
-  elif (output == 'gammaxy'):
+  elif (output == 'gammaxy' or output == 'epsth'):
     return eps[2]
+  elif (output == 'gammarz' and type2D == 'axisymmetric'):
+    return eps[3]
   else:
     print('Error in Q4_strain: output not recognized')
     raise Exception
